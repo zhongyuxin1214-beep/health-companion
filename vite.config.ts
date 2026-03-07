1102a4526143feae255f25966a92792718d9bed4
zhongyuxin1214-beep <zhongyuxin1214@gmail.com>
2026-03-07 11:25:31 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "./",
   server: {
     host: "::",
     port: 8080,
@@ -19,3 +20,4 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+
